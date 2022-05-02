deba4cc0801db710ba500008b954e69884ea316e
Sine Zwane <njabulosbzwane25@gmail.com>
2022-05-02 10:57:58 +0200

    explore edit

--- frontend/src/navigation/pages/Explore.jsx
+++ frontend/src/navigation/pages/Explore.jsx
@@ -1,12 +1,162 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import Search from './Search';
 
-export default function Explore({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Explore</Text>
-        </View>
-    );
+export default function ExploreByCategory() {
+    
+  return (
+      
+    <div className="explore-by-cat-container">
+        <Search />
+
+        <h1 className="category-header">Explore by Category</h1>
+        <ul className="explore-by-category-button-list">
+
+        <button className="cat-1"><p>Women's Fashion</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+        }
+        </button>
+       <button className="cat-1"><p>Men's Fashion</p> {
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Fashion and Clothing</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Gaming and Computers</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Kids Fashion and Clothing</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Home and Household</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Consumer Electronics</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Books and Stationary</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Cosmetics and Beauty</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Accessories and Gadgets</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Appliances and Home</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Sports and Outdoors</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Baby, Toys and Children</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Art and Supplies</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>D.I.Y and Tools</p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+    </button>
+        <button className="cat-1"><p>Other and Uncategorised </p>{
+            
+            <p><img 
+                className="arrow-img"
+                src="/next.png" 
+                alt="right-arrow" />
+                </p>
+    }
+</button>
+        </ul>
+
+    </div>
+  )
 }
